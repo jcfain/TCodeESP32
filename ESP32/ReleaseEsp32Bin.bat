d70f01061b76e2face7f3ab2b27412f318a6054b
Jason Fain <jfain878@gmail.com>
2021-04-28 15:26:43 -0400

    Deploy batch now overwrites Updating installation pdf

--- ESP32/ReleaseEsp32Bin.bat
+++ ESP32/ReleaseEsp32Bin.bat
@@ -1,14 +1,14 @@
 REM 0x1000
-echo F|XCOPY %userprofile%\.platformio\packages\framework-arduinoespressif32\tools\sdk\bin\bootloader_dio_40m.bin %~dp0bin\Release\a_0x1000.bin /d
+echo F|XCOPY %userprofile%\.platformio\packages\framework-arduinoespressif32\tools\sdk\bin\bootloader_dio_40m.bin %~dp0bin\Release\a_0x1000.bin /d /Y
 REM 0x8000
-echo F|XCOPY .pio\build\esp32doit-devkit-v1\partitions.bin %~dp0bin\Release\b_0x8000.bin /d
+echo F|XCOPY .pio\build\esp32doit-devkit-v1\partitions.bin %~dp0bin\Release\b_0x8000.bin /d /Y
 REM 0x10000 
-echo F|XCOPY .pio\build\esp32doit-devkit-v1\firmware.bin %~dp0bin\Release\c_0x10000.bin /d
+echo F|XCOPY .pio\build\esp32doit-devkit-v1\firmware.bin %~dp0bin\Release\c_0x10000.bin /d /Y
 REM 0xe000 
-echo F|XCOPY %userprofile%\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin %~dp0bin\Release\d_0xe000.bin /d
+echo F|XCOPY %userprofile%\.platformio\packages\framework-arduinoespressif32\tools\partitions\boot_app0.bin %~dp0bin\Release\d_0xe000.bin /d /Y
 REM 0x003d0000
-echo F|XCOPY .pio\build\esp32doit-devkit-v1\spiffs.bin %~dp0bin\Release\e_0x003d0000.bin /d
+echo F|XCOPY .pio\build\esp32doit-devkit-v1\spiffs.bin %~dp0bin\Release\e_0x003d0000.bin /d /Y
 
-echo F|XCOPY "..\How to upload binaries.pdf" "%~dp0bin\Release\How to upload binaries.pdf" /d
+XCOPY "..\How to upload binaries.pdf" "%~dp0bin\Release\How to upload binaries.pdf" /d /Y
 
 pause
