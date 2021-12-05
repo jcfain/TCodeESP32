4f9c9c3d5bd48e71d9bd2c991af741892479a1ac
Jason Fain <jfain878@gmail.com>
2021-12-04 21:37:10 -0500

    Adding analog twist servo support. (Untested so use at your own risk) Convert web js to raw javascript for IOS devices.

--- ESP32/src/WebHandler.h
+++ ESP32/src/WebHandler.h
@@ -31,6 +31,7 @@ SOFTWARE. */
 #include "WifiHandler.h"
 
 AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 class WebHandler {
     private:
@@ -60,47 +61,15 @@ class WebHandler {
             // Serial.println(port);
             // server = AsyncWebServer(port);
 
-            // This has issues running with the webserver.
             if(!apMode)
             {
                 startMDNS(hostName, friendlyName);
             }
-
-            // Route for root / web page
-            // if (apMode) 
-            // {
-            //     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            //     {
-            //         request->send(SPIFFS, "/www/wifiSettings.htm");
-            //     });
-            // }
-            // else 
-            // {
-            //     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            //     {
-            //         request->send(SPIFFS, "/www/index.htm");
-            //     });
-            // }
-
-            // server.on("/wifiSettings", HTTP_GET, [](AsyncWebServerRequest *request)
-            // {
-            //     request->send(SPIFFS, "/www/wifiSettings.htm");
+            // communicate via websocket.
+            // ws.onEvent([&](AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
+            //     onWsEvent(server, client, type, arg, data, len);
             // });
-
-            // server.on("/jquery", HTTP_GET, [](AsyncWebServerRequest *request)
-            // {
-            //     request->send(SPIFFS, "/www/jquery-3.5.1.min.js");
-            // });
-
-            // server.on("/settings", HTTP_GET, [](AsyncWebServerRequest *request) 
-            // {
-            //     request->send(SPIFFS, "/www/settings.js");
-            // });
-
-            // server.on("/style", HTTP_GET, [](AsyncWebServerRequest *request) 
-            // {
-            //     request->send(SPIFFS, "/www/style.css", "text/css");
-            // });
+            // server.addHandler(&ws);
 
             server.on("/userSettings", HTTP_GET, [](AsyncWebServerRequest *request) 
             {
@@ -260,5 +229,103 @@ class WebHandler {
                 MDNS.addService("http", "tcp", 80);
                 MDNS.addService("tcode", "udp", SettingsHandler::udpServerPort);
                 MDNSInitialized = true;
+            }
+            
+            void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len)
+            {
+                if(type == WS_EVT_CONNECT)
+                {
+                    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+                    client->printf("Hello Client %u :)", client->id());
+                    client->ping();
+                } 
+                else if(type == WS_EVT_DISCONNECT)
+                {
+                    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+                } 
+                else if(type == WS_EVT_ERROR)
+                {
+                    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+                } 
+                else if(type == WS_EVT_PONG)
+                {
+                    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+                } 
+                else if(type == WS_EVT_DATA)
+                {
+                    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+                    String msg = "";
+                    if(info->final && info->index == 0 && info->len == len)
+                    {
+                        //the whole message is in a single frame and we got all of it's data
+                        Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+
+                        if(info->opcode == WS_TEXT)
+                        {
+                            for(size_t i=0; i < info->len; i++) 
+                            {
+                                msg += (char) data[i];
+                            }
+                        } 
+                        else 
+                        {
+                            char buff[3];
+                            for(size_t i=0; i < info->len; i++) 
+                            {
+                                sprintf(buff, "%02x ", (uint8_t) data[i]);
+                                msg += buff ;
+                            }
+                        }
+                        Serial.printf("%s\n",msg.c_str());
+
+                        if(info->opcode == WS_TEXT)
+                            client->text("I got your text message");
+                        else
+                            client->binary("I got your binary message");
+                    } 
+                    else 
+                    {
+                    //message is comprised of multiple frames or the frame is split into multiple packets
+                        if(info->index == 0)
+                        {
+                            if(info->num == 0)
+                                Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+                            Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+                        }
+
+                        Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+
+                        if(info->opcode == WS_TEXT)
+                        {
+                            for(size_t i=0; i < len; i++) 
+                            {
+                                msg += (char) data[i];
+                            }
+                        } 
+                        else 
+                        {
+                            char buff[3];
+                            for(size_t i=0; i < len; i++) 
+                            {
+                                sprintf(buff, "%02x ", (uint8_t) data[i]);
+                                msg += buff ;
+                            }
+                        }
+                        Serial.printf("%s\n",msg.c_str());
+
+                        if((info->index + len) == info->len)
+                        {
+                            Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+                            if(info->final)
+                            {
+                                Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+                                if(info->message_opcode == WS_TEXT)
+                                    client->text("I got your text message");
+                                else
+                                    client->binary("I got your binary message");
+                            }
+                        }
+                    }
+                }
             }
 };
