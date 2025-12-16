383860aa71e5a71171a25671c2f00f8f07287e4c
Fain <25233821+jcfain@users.noreply.github.com>
2025-12-16 16:29:09 -0500

    Update constants.h

--- ESP32/lib/constants.h
+++ ESP32/lib/constants.h
@@ -2,7 +2,7 @@
 
 #include "struct/channel.h"
 
-#define FIRMWARE_VERSION 0.48f
+#define FIRMWARE_VERSION 0.481f
 #define FIRMWARE_VERSION_NAME "0.481a\n"
 #define MAX_BUTTON_SETS 4
 #define MAX_BUTTONS 4
